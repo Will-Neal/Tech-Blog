1ba1f9913d4535f1fbae3648be6095e708cb162e
Will Neal <willwneal@gmail.com>
2022-03-02 16:36:14 -0500

    Added ability to post and render comments on each post

--- controllers/articles.js
+++ controllers/articles.js
@@ -1,13 +1,31 @@
 const express = require('express');
-const { Post } = require('../models');
+const { Post, Comment } = require('../models');
 const router = express.Router();
 
 router.get('/new', (req, res) => {
     res.render('new') 
 })
 
+router.get('/:id', async (req, res) => {
+    id = req.params.id
+    try{
+       const singlePost = await Post.findOne({
+           where: { id }
+       }) 
+       const cleanPost = singlePost.get({ plain:true })
+       const commentsObj = await Comment.findAll({
+           where: {post_id:id}
+       })
+       const comments = commentsObj.map((comment) => comment.get({ plain:true }))
+       //Need to figure out why they are showing up as
+       res.render('single', {cleanPost, comments}) 
+    } catch(err) {
+        console.log(err)
+    }
+    
+})
+
 router.post('/new', (req, res) => {
-    console.log(req.body)
     Post.create({
         title: req.body.title,
         subject: req.body.subject,
@@ -19,6 +37,18 @@ router.post('/new', (req, res) => {
       .catch((err) => {
           res.redirect('/')
       })
+})
+
+router.post('/comment/:id', (req, res) => {
+    Comment.create({
+        comment: req.body.comment,
+        post_id: req.params.id
+    })
+    .then((newComment) => {
+        console.log(newComment);
+        res.redirect(`/articles/${req.params.id}`)
+        
+    })
 })
 
 module.exports = router
