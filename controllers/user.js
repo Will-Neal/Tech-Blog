4d766b064905f3508840746355d666393aad8a5f
Will Neal <willwneal@gmail.com>
2022-03-03 09:13:49 -0500

    Users now being added to database with hashed password

--- controllers/user.js
+++ controllers/user.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { append } = require('express/lib/response');
 const { Post, Comment, User } = require('../models');
 const router = express.Router();
+const bcrypt = require('bcrypt')
 
 router.get('/login', (req, res) => {
     res.render('login')
@@ -11,5 +12,22 @@ router.get('/register', (req, res) => {
     res.render('register')
 })
 
+router.post('/login', (req, res) => {
+
+})
+
+router.post('/register', async (req, res) => {
+    try{
+        const hashedPassword = await bcrypt.hash(req.body.password, 10)
+        await User.create({
+            name: req.body.name,
+            email: req.body.email,
+            password: hashedPassword 
+        }) 
+        res.redirect('/user/login')
+    } catch {
+        res.redirect('/user/register')
+    }
+})
 
 module.exports = router;
